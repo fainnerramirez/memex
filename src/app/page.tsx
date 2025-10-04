e4a1598551e9db78b398013b0160a349d6194738
fainnerramirez <fainnerramirez@gmail.com>
2025-10-03 19:48:02 -0500

    feat: add fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+'use client'
 import { Button, HeroUIProvider } from "@heroui/react";
 
 export default function Home() {
   return (
     <HeroUIProvider>
-      <Button color="warning">Hola mundo</Button>
+      <main className="grid place-content-center min-h-screen">
+        <Button color="secondary" variant="solid" >Hola mundo</Button>
+      </main>
     </HeroUIProvider>
   );
 }
